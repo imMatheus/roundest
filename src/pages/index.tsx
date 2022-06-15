92fe2be9108c9e36b2aa6fb73bd832d48a2b4205
imMatheus <matheusmendesbarata03@gmail.com>
2022-06-15 18:21:40 +0200

    fixed url error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,16 @@ import { getOptionsForVote } from '@/utils/getRandomPokemon'
 import { useState } from 'react'
 
 const Home: NextPage = () => {
-    const [[first, second]] = useState(getOptionsForVote())
+    const [[first, second], updateId] = useState(getOptionsForVote())
 
     const firstPokemon = trpc.useQuery(['get-pokemon-by-id', { id: first }])
     const secondPokemon = trpc.useQuery(['get-pokemon-by-id', { id: second }])
-    console.log(firstPokemon)
 
     if (firstPokemon.isLoading || secondPokemon.isLoading) return null
+
+    const voteForRoundest = (selected: number) => {
+        updateId(getOptionsForVote())
+    }
 
     return (
         <div className='h-screen w-screen justify-center flex flex-col'>
@@ -18,26 +21,40 @@ const Home: NextPage = () => {
                 Which pokemon is rounder?
             </div>
             <div className='border mt-4 rounded p-8 flex justify-between max-w-2xl mx-auto w-full items-center'>
-                <div className='w-64 h-64 flex flex-col'>
+                <div className='w-64 flex flex-col'>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
                         src={firstPokemon.data?.sprites.front_default || ''}
                         alt=''
-                        className='w-full'
+                        className='w-full h-64'
                     />
                     <div className='text-xl text-center capitalize'>
                         {firstPokemon.data?.name}
                     </div>
+                    <button
+                        className='bg-indigo-700 block py-1.5 rounded-md px-2'
+                        onClick={() => voteForRoundest(first)}
+                    >
+                        Rounder
+                    </button>
                 </div>
                 <div>vs</div>
-                <div className='w-64 h-64 flex flex-col'>
+                <div className='w-64 flex flex-col'>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
                         src={secondPokemon.data?.sprites.front_default || ''}
                         alt=''
-                        className='w-full'
+                        className='w-full h-64'
                     />
                     <div className='text-xl text-center capitalize'>
                         {secondPokemon.data?.name}
                     </div>
+                    <button
+                        className='bg-indigo-700 block py-1.5 rounded-md px-2'
+                        onClick={() => voteForRoundest(second)}
+                    >
+                        Rounder
+                    </button>
                 </div>
             </div>
         </div>
